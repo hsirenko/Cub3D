ab0377517803b8c212a0307f4cac8ac45e78914b
Ksenia Karemina <kseniakaremina@MacBook-Pro-Ksenia.local>
2024-10-07 17:48:14 +0200

    fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kseniakaremina <kseniakaremina@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:04:32 by helensirenk       #+#    #+#             */
-/*   Updated: 2024/10/07 17:44:33 by kseniakarem      ###   ########.fr       */
+/*   Updated: 2024/10/07 17:47:22 by kseniakarem      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
 
     init_struct_game(&game, argv[1]);
     game.mlx = mlx_init();
-    game.win = mlx_new_window(&game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3D");
+    game.win = mlx_new_window(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3D");
     game.img.img = mlx_new_image(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
     game.img.addr = mlx_get_data_addr(game.img.img, &game.img.bpp, &game.img.line_length, &game.img.endian);
     //my_mlx_pixel_put(img, 5, 5, RED);
