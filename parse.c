be366fca23285515f1483ab4cbe41df3013479e1
Ksenia Karemina <kseniakaremina@MacBook-Pro-Ksenia.local>
2024-10-14 19:52:57 +0200

    norm

--- parse.c
+++ parse.c
@@ -1,219 +1,275 @@
 #include "cub3D.h"
 
-void panic(char* msg) {
-    printf("Error: %s\n", msg);
-    char *c = 0;
-    *c = 0;
-}
-
-char *read_file(int fd)
+void	panic(char *msg)
 {
-    int capacity = 1024;
-    int length = 0;
-    char *str = ft_calloc(capacity, sizeof(char) + 1);
+	char	*c;
 
-    while(1) {
-        int bytes_read = read(fd, &str[length], capacity - length);
-        if (bytes_read < 0) {
-            free(str);
-            panic("read call failed");
-            return NULL;
-        }
-        if (!bytes_read) {
-            return str;
-        }
-        length += bytes_read;
-        if (length == capacity) {
-            char* s = ft_calloc(capacity * 2 + 1, sizeof(char));
-            ft_memcpy(s, str, capacity);
-            capacity *= 2;
-            free(str);
-            str = s;
-        }
-    }
-    panic("unreachable file");
+	printf("Error: %s\n", msg);
+	c = 0;
+	*c = 0;
 }
 
-int find_prefix(char *prefix, char * string)
-{   
-    while (*prefix)
-    {
-        if (*prefix == *string)
-        {
-            prefix++;
-            string++;
-        }
-        else
-            return(0);
-    }
-    return(1);
-}
-
-
-char* trim_str(int len, char* str) {
-    return &str[len];
-}
-
-
-int parse_path(void* mlx, char* direction, char **inp_string, t_image* img)
+typedef struct s_string
 {
-    char *path;
-    char *end_path;
+	int		len;
+	int		cap;
+	char	*data;
+}			t_string;
 
-    if (!find_prefix(direction, *inp_string))
-        return 0;
-    *inp_string = trim_str(ft_strlen(direction), *inp_string);
-    end_path = ft_strchr(*inp_string, '\n');
-    path = ft_substr(*inp_string,0,(end_path-*inp_string));
-    *inp_string = trim_str(ft_strlen(path) + 1, *inp_string);
-    // printf("mlx=%p path_ptr=%p path=%s img=%p\n", mlx, path, path, img);
-    img->img = mlx_xpm_file_to_image(mlx, path, &img->img_w, &img->img_h);
-    if(!img->img) {
-        return(0);
-    }
-    return(1);
-}
-
-int get_wall_img( t_game* game, char **inp_string)
+t_string	string_with_capacity(int cap)
 {
-    int no = parse_path(game->mlx, "NO ", inp_string, &game->mapdata.north_wall);
-    if (!no)
-        return 0;
-    int so = parse_path(game->mlx, "SO ", inp_string, &game->mapdata.south_wall);
-    if (!so)
-        return 0;
-    int we = parse_path(game->mlx, "WE ", inp_string, &game->mapdata.west_wall);
-    if (!we)
-        return 0;
-    int ea = parse_path(game->mlx, "EA ", inp_string, &game->mapdata.east_wall);
-    if (!ea)
-        return 0;
-    return 1;
+	t_string	s;
+
+	s.cap = cap;
+	s.len = 0;
+	s.data = ft_calloc(cap + 1, sizeof(char));
+	return (s);
 }
 
-char* cut_string(char * string, int n )
+t_string	string_grow(t_string s, int new_cap)
 {
-    char* part = ft_calloc(n, sizeof(char));
-    int i = 0;
+	t_string	res;
 
-    while (i < n) {
-        part[i] = string[i];
-        i++;
-    }
-    return part;
+	res.cap = new_cap;
+	res.data = ft_calloc(new_cap + 1, sizeof(char));
+	res.len = s.len;
+	ft_memcpy(res.data, s.data, s.len);
+	return (res);
 }
 
-// validate that the first n characters are digits
-// and parse them into an int. Return -1 on error or 
-// if n is oustide the [0; 255] range
-int parse_byte_n(char* s, int n) {
-    int res = 0;
-    int i = 0;
-    while(i < n) {
-        if (!ft_isdigit(s[i]))
-            return -1;
-        res = res * 10 + (s[i] - '0');
-        i++;
-    }
-    if (res < 0 || res > 255)
-        return -1;
-    return res;
-}
-
-int index_of(char* s, char c) {
-    int i = 0;
-    if (!s)
-        return -1;
-    while(s[i]) {
-        if (s[i] == c)
-            return i;
-        i++;
-    }
-    return -1;
-}
-
-int read_color(char** str, char sep) {
-    int sep_pos = index_of(*str, sep);
-    if (sep_pos == -1)
-        return -1;
-    int res = parse_byte_n(*str, sep_pos);
-    if (res == -1)
-        return -1;
-    *str += sep_pos + 1;
-    return res;
-}
-
-int read_colors(char **str)
+char	*read_file(int fd)
 {
-    int red = read_color(str, ',');
-    int green = read_color(str, ',');
-    int blue = read_color(str, '\n');
-    if (red == -1 || blue == -1 || green == -1)
-        return -1;
-    return red << 16 | green << 8 | blue;
+	int			bytes_read;
+	char		*s;
+	t_string	str;
+
+	str = string_with_capacity(1024);
+	while (1)
+	{
+		bytes_read = read(fd, &str.data[str.len], str.cap - str.len);
+		if (bytes_read < 0)
+		{
+			free(str.data);
+			panic("read call failed");
+			return (NULL);
+		}
+		if (!bytes_read)
+		{
+			return (str.data);
+		}
+		str.len += bytes_read;
+		if (str.len == str.cap)
+			str = string_grow(str, str.cap * 2);
+	}
+	panic("unreachable file");
 }
 
-int get_colors(t_game* game, char **inp_string)
+int	find_prefix(char *prefix, char *string)
 {
-    if((*inp_string)[0]== '\n')
-        *inp_string = trim_str(1, *inp_string);
-    if ((*inp_string)[0] == 'F')
-    {
-        *inp_string = trim_str(2, *inp_string);
-        game->color_floor = read_colors(inp_string);
-        if (game->color_floor == -1)
-            return 0;
-    }
-    if ((*inp_string)[0] == 'C')
-    {
-        *inp_string = trim_str(2, *inp_string);
-        game->color_ceiling = read_colors(inp_string);
-        if (game->color_ceiling == -1)
-            return 0;
-    }
-    return(1);
+	while (*prefix)
+	{
+		if (*prefix == *string)
+		{
+			prefix++;
+			string++;
+		}
+		else
+			return (0);
+	}
+	return (1);
 }
 
-int get_map_height(char *inp_string)
+char	*trim_str(int len, char *str)
 {
-    int h = 0;
-    while (*inp_string)
-    {
-        if(*inp_string=='\n')
-            h++;
-        inp_string++;
-    }
-    return(h+1);
+	return (&str[len]);
 }
 
-int get_map_width(char *inp_string)
+int	parse_path(void *mlx, char *direction, char **inp_string, t_image *img)
 {
-    int max_len=0;
-    while(*inp_string)
-    {
-        int len=0;
-        while((*inp_string!='\n') && (*inp_string))
-        {
-            len++;
-            inp_string++;
-        }
-        if (max_len < len)
-            max_len = len;
-        if (*inp_string)
-            inp_string++;
-    }
-    return(max_len);
+	char	*path;
+	char	*end_path;
+
+	if (!find_prefix(direction, *inp_string))
+		return (0);
+	*inp_string = trim_str(ft_strlen(direction), *inp_string);
+	end_path = ft_strchr(*inp_string, '\n');
+	path = ft_substr(*inp_string, 0, (end_path - *inp_string));
+	*inp_string = trim_str(ft_strlen(path) + 1, *inp_string);
+	img->img = mlx_xpm_file_to_image(mlx, path, &img->img_w, &img->img_h);
+	if (!img->img)
+	{
+		return (0);
+	}
+	return (1);
 }
 
-int check(char c, char *str)
+int	get_wall_img(t_game *game, char **inp_string)
 {
-    while (*str)
-    {
-        if (*str == c)
-        return(1);
-        str++;
-    }
-    return(0);
+	int	no;
+	int	so;
+	int	we;
+	int	ea;
+
+	no = parse_path(game->mlx, "NO ", inp_string, &game->mapdata.north_wall);
+	if (!no)
+		return (0);
+	so = parse_path(game->mlx, "SO ", inp_string, &game->mapdata.south_wall);
+	if (!so)
+		return (0);
+	we = parse_path(game->mlx, "WE ", inp_string, &game->mapdata.west_wall);
+	if (!we)
+		return (0);
+	ea = parse_path(game->mlx, "EA ", inp_string, &game->mapdata.east_wall);
+	if (!ea)
+		return (0);
+	return (1);
+}
+
+char	*cut_string(char *string, int n)
+{
+	char	*part;
+	int		i;
+
+	part = ft_calloc(n, sizeof(char));
+	i = 0;
+	while (i < n)
+	{
+		part[i] = string[i];
+		i++;
+	}
+	return (part);
+}
+
+int	parse_byte_n(char *s, int n)
+{
+	int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
+	while (i < n)
+	{
+		if (!ft_isdigit(s[i]))
+			return (-1);
+		res = res * 10 + (s[i] - '0');
+		i++;
+	}
+	if (res < 0 || res > 255)
+		return (-1);
+	return (res);
+}
+
+int	index_of(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	read_color(char **str, char sep)
+{
+	int	sep_pos;
+	int	res;
+
+	sep_pos = index_of(*str, sep);
+	if (sep_pos == -1)
+		return (-1);
+	res = parse_byte_n(*str, sep_pos);
+	if (res == -1)
+		return (-1);
+	*str += sep_pos + 1;
+	return (res);
+}
+
+int	read_colors(char **str)
+{
+	int	red;
+	int	green;
+	int	blue;
+
+	red = read_color(str, ',');
+	green = read_color(str, ',');
+	blue = read_color(str, '\n');
+	if (red == -1 || blue == -1 || green == -1)
+		return (-1);
+	return (red << 16 | green << 8 | blue);
+}
+
+int	get_colors(t_game *game, char **inp_string)
+{
+	if ((*inp_string)[0] == '\n')
+		*inp_string = trim_str(1, *inp_string);
+	if ((*inp_string)[0] == 'F')
+	{
+		*inp_string = trim_str(2, *inp_string);
+		game->color_floor = read_colors(inp_string);
+		if (game->color_floor == -1)
+			return (0);
+	}
+	if ((*inp_string)[0] == 'C')
+	{
+		*inp_string = trim_str(2, *inp_string);
+		game->color_ceiling = read_colors(inp_string);
+		if (game->color_ceiling == -1)
+			return (0);
+	}
+	return (1);
+}
+
+int	get_map_height(char *inp_string)
+{
+	int	h;
+
+	h = 0;
+	while (*inp_string)
+	{
+		if (*inp_string == '\n')
+			h++;
+		inp_string++;
+	}
+	return (h + 1);
+}
+
+int	get_map_width(char *inp_string)
+{
+	int	max_len;
+	int	len;
+
+	max_len = 0;
+	while (*inp_string)
+	{
+		len = 0;
+		while ((*inp_string != '\n') && (*inp_string))
+		{
+			len++;
+			inp_string++;
+		}
+		if (max_len < len)
+			max_len = len;
+		if (*inp_string)
+			inp_string++;
+	}
+	return (max_len);
+}
+
+int	check(char c, char *str)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
 }
 
 void	find_angle_view(t_game *game, const char c)
@@ -228,113 +284,123 @@ void	find_angle_view(t_game *game, const char c)
 		game->player.angle = -0.5f * M_PI;
 }
 
-int check_gamer(char gamer, int gamer_count)
+int	check_gamer(char gamer, int gamer_count)
 {
-    if (gamer_count != 1)
-    {
-        panic("insufficient amount of gamers");
-        return(0);
-    }
-    if (!check(gamer,"NSWE"))
-    {
-        panic("insuffifient gamer");
-        return(0);
-    }
-    return(1);
+	if (gamer_count != 1)
+	{
+		panic("insufficient amount of gamers");
+		return (0);
+	}
+	if (!check(gamer, "NSWE"))
+	{
+		panic("insuffifient gamer");
+		return (0);
+	}
+	return (1);
 }
 
-int fill_map(t_game *game, char* src)
+int	fill_map(t_game *game, char *src)
 {
-    char gamer;
-    int gamer_count = 0;
-    int i=0;
-    int j=0;
-    int c = 0;
+	char	gamer;
+	int		gamer_count;
+	int		i;
+	int		j;
+	int		c;
 
-    while(src[c])
-    {
-        if (src[c] == '\n')  {
-            i++;
-            j = 0;
-            c++;
-            continue;
-        }
-        if (src[c] == '1')
-            game->mapdata.map2d[i][j] = '1';
-        else 
-            game->mapdata.map2d[i][j] = '0';
-        
-        if (!check(src[c], " 01")) {
-            gamer_count++;
-            gamer = src[c];
-            game->player.player_x = j + 0.5;
-            game->player.player_y = i + 0.5;
-        }
-        j++;
-        c++;
-    }
-    if (!check_gamer(gamer, gamer_count))
-        return(0);
-    game->player.start_orient = gamer;
-    find_angle_view(game,gamer);
-    return(1); 
+	gamer_count = 0;
+	i = 0;
+	j = 0;
+	c = 0;
+	while (src[c])
+	{
+		if (src[c] == '\n')
+		{
+			i++;
+			j = 0;
+			c++;
+			continue ;
+		}
+		if (src[c] == '1')
+			game->mapdata.map2d[i][j] = '1';
+		else
+			game->mapdata.map2d[i][j] = '0';
+		if (!check(src[c], " 01"))
+		{
+			gamer_count++;
+			gamer = src[c];
+			game->player.player_x = j + 0.5;
+			game->player.player_y = i + 0.5;
+		}
+		j++;
+		c++;
+	}
+	if (!check_gamer(gamer, gamer_count))
+		return (0);
+	game->player.start_orient = gamer;
+	find_angle_view(game, gamer);
+	return (1);
 }
 
-
-int get_map(t_game *game, char **inp_string)
+int	get_map(t_game *game, char **inp_string)
 {
-    if((*inp_string)[0]== '\n')
-    *inp_string = trim_str(1, *inp_string);
-    game->mapdata.map_h = get_map_height(*inp_string);
-    game->mapdata.map_w = get_map_width(*inp_string);
-    game->mapdata.map2d = ft_calloc(game->mapdata.map_h,sizeof(char*));
-    int i = 0;
-    while(i < game->mapdata.map_h) {
-        game->mapdata.map2d[i] = ft_calloc(game->mapdata.map_w, sizeof(char));
-        ft_memset(game->mapdata.map2d[i], '0', game->mapdata.map_w);
-        i++;
-    }
-    fill_map(game, *inp_string);
-    return(1);
+	int	i;
+
+	if ((*inp_string)[0] == '\n')
+		*inp_string = trim_str(1, *inp_string);
+	game->mapdata.map_h = get_map_height(*inp_string);
+	game->mapdata.map_w = get_map_width(*inp_string);
+	game->mapdata.map2d = ft_calloc(game->mapdata.map_h, sizeof(char *));
+	i = 0;
+	while (i < game->mapdata.map_h)
+	{
+		game->mapdata.map2d[i] = ft_calloc(game->mapdata.map_w, sizeof(char));
+		ft_memset(game->mapdata.map2d[i], '0', game->mapdata.map_w);
+		i++;
+	}
+	fill_map(game, *inp_string);
+	return (1);
 }
 
+int	parse(t_game *game, int fd)
+{
+	char	*inp_string;
 
-int parse(t_game* game, int fd) {
-    char *inp_string = read_file(fd);
-    if(!get_wall_img(game, &inp_string))
-    {
-        panic( "could not parse image paths");
-        return(0);
-    }
-    if(!(get_colors(game,&inp_string)==1))
-        {
-        panic( "could not parse floor and ceiling colors");
-        return(0);
-    }
-    if(!(get_map(game,&inp_string)))
-    {
-        panic("could not get map or it's invalid");
-        return(0);
-    }
-    return 1;
+	inp_string = read_file(fd);
+	if (!get_wall_img(game, &inp_string))
+	{
+		panic("could not parse image paths");
+		return (0);
+	}
+	if (!(get_colors(game, &inp_string) == 1))
+	{
+		panic("could not parse floor and ceiling colors");
+		return (0);
+	}
+	if (!(get_map(game, &inp_string)))
+	{
+		panic("could not get map or it's invalid");
+		return (0);
+	}
+	return (1);
 }
 
-void init_struct_game(t_game *game, char * file_name)
+void	init_struct_game(t_game *game, char *file_name)
 {
-    (void)game;
-    int fd = open(file_name, O_RDONLY);
-    if (fd < 0) {
-        panic("could not open file");
-    }
+	(void)game;
+	int fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+	{
+		panic("could not open file");
+	}
 
-    int status = parse(game, fd);
-    if (status == 0) {
-        panic("failed to parse map");
-    }
+	int status = parse(game, fd);
+	if (status == 0)
+	{
+		panic("failed to parse map");
+	}
 
-    if (has_holes(game->mapdata, game->player)) {
-        panic("map has holes");
-    }
-
-    // printf("map parsed width=%d height=%d\n", game->mapdata.map_w, game->mapdata.map_h);
+	if (has_holes(game->mapdata, game->player))
+	{
+		panic("map has holes");
+	}
 }
--- render.c
+++ render.c
@@ -1,6 +1,6 @@
 #include "cub3D.h"
 
-void my_mlx_pixel_put(t_img* img, int x, int y, int color) // put the pixel
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color) // put the pixel
 {
 	if (x < 0) // check the x position
 		return ;
@@ -10,19 +10,22 @@ void my_mlx_pixel_put(t_img* img, int x, int y, int color) // put the pixel
 		return ;
 	else if (y >= SCREEN_HEIGHT)
 		return ;
-    unsigned int* pixel_addr = (unsigned int*)(img->addr + img->line_length * y + img->bpp/8 * x);
-    *pixel_addr = color;
+	unsigned int *pixel_addr = (unsigned int *)(img->addr + img->line_length * y
+			+ img->bpp / 8 * x);
+	*pixel_addr = color;
 }
 
-void draw_floor_ceiling(t_game *game, int ray_counter, int t_pix, int b_pix) // draw the floor and the ceiling
+void	draw_floor_ceiling(t_game *game, int ray_counter, int t_pix, int b_pix)
+	// draw the floor and the ceiling
 {
- int  i =b_pix;
+	int i = b_pix;
 
 	while (i < SCREEN_HEIGHT)
-    {
-        my_mlx_pixel_put(&game->img, ray_counter, i++, game->color_floor); // floor
-    }
-	i=0;
+	{
+		my_mlx_pixel_put(&game->img, ray_counter, i++, game->color_floor);
+			// floor
+	}
+	i = 0;
 	while (i < t_pix)
 		my_mlx_pixel_put(&game->img, ray_counter, i++, game->color_ceiling);
 }
@@ -30,40 +33,46 @@ void draw_floor_ceiling(t_game *game, int ray_counter, int t_pix, int b_pix) // 
 t_image	*get_texture(t_game *game, t_direction face)
 {
 	if (face == DIRECTION_EAST)
-		return &game->mapdata.east_wall;
+		return (&game->mapdata.east_wall);
 	if (face == DIRECTION_WEST)
-		return &game->mapdata.west_wall;
+		return (&game->mapdata.west_wall);
 	if (face == DIRECTION_SOUTH)
-		return &game->mapdata.south_wall;
-	return &game->mapdata.north_wall;
+		return (&game->mapdata.south_wall);
+	return (&game->mapdata.north_wall);
 }
 
-int get_colour(t_direction face)
+int	get_colour(t_direction face)
 {
 	if (face == DIRECTION_EAST)
-		return PSYCHEDELIC_PINK;
+		return (PSYCHEDELIC_PINK);
 	if (face == DIRECTION_WEST)
-		return PURPLE;
+		return (PURPLE);
 	if (face == DIRECTION_SOUTH)
-		return PSYCHEDELIC_LIME;
-	return BLUE;
+		return (PSYCHEDELIC_LIME);
+	return (BLUE);
 }
 
-unsigned int my_mlx_pixel_get(t_image img, int x, int y) {
-	int bits_per_pixel, line_size, endian;
-	char* addr = mlx_get_data_addr(img.img, &bits_per_pixel, &line_size, &endian);
-	return *(unsigned int*)(&addr[y * line_size + x * bits_per_pixel / 8]);
-}
-
-void	draw_wall(t_game * game, int ray_counter, int t_pix, int b_pix, t_hit hit)	// draw the wall
+unsigned int	my_mlx_pixel_get(t_image img, int x, int y)
 {
-	double			x_o;
-	double			y_o;
-	t_image	*texture;
-	double			factor;
+	char	*addr;
+
+	int bits_per_pixel, line_size, endian;
+	addr = mlx_get_data_addr(img.img, &bits_per_pixel, &line_size, &endian);
+	return (*(unsigned int *)(&addr[y * line_size + x * bits_per_pixel / 8]));
+}
+
+void	draw_wall(t_game *game, int ray_counter, int t_pix, int b_pix,
+		t_hit hit) // draw the wall
+{
+	double x_o;
+	double y_o;
+	t_image *texture;
+	double factor;
 
 	texture = get_texture(game, hit.face);
-	double wall_h = (1. / hit.distance) * ((SCREEN_WIDTH / 2) / tan(game->player.fov_radians / 2)); // get the wall height b_pix - t_pix;
+	double wall_h = (1. / hit.distance) * ((SCREEN_WIDTH / 2)
+			/ tan(game->player.fov_radians / 2)); // get the wall height b_pix
+		- t_pix;
 	factor = (double)texture->img_h / wall_h;
 	x_o = hit.local_x * texture->img_w;
 	y_o = (t_pix - (SCREEN_HEIGHT / 2) + (wall_h / 2)) * factor;
@@ -71,27 +80,32 @@ void	draw_wall(t_game * game, int ray_counter, int t_pix, int b_pix, t_hit hit)	
 		y_o = 0;
 	while (t_pix < b_pix)
 	{
-		my_mlx_pixel_put(&game->img, ray_counter, t_pix, my_mlx_pixel_get(*texture, x_o, y_o));
+		my_mlx_pixel_put(&game->img, ray_counter, t_pix,
+				my_mlx_pixel_get(*texture, x_o, y_o));
 		y_o += factor;
 		t_pix++;
 	}
 }
 
-void render_wall(t_game *game, int ray_counter, t_hit hit) // render the wall
+void	render_wall(t_game *game, int ray_counter, t_hit hit) // render the wall
 {
 	double wall_h;
 	double b_pix;
 	double t_pix;
 
-	hit.distance *= cos(nor_angle(hit.angle - game->player.angle)); // fix the fisheye
-	wall_h = (1. / hit.distance) * ((SCREEN_WIDTH / 2) / tan(game->player.fov_radians / 2)); // get the wall height
+	hit.distance *= cos(nor_angle(hit.angle
+				- game->player.angle));                         
+		// fix the fisheye
+	wall_h = (1. / hit.distance) * ((SCREEN_WIDTH / 2)
+			/ tan(game->player.fov_radians / 2)); // get the wall height
 	// printf("dist=%f wall_h=%f\n", game->ray.dist, wall_h);
 	b_pix = (SCREEN_HEIGHT / 2) + (wall_h / 2); // get the bottom pixel
 	t_pix = (SCREEN_HEIGHT / 2) - (wall_h / 2); // get the top pixel
-	if (b_pix >SCREEN_HEIGHT) // check the bottom pixel
+	if (b_pix > SCREEN_HEIGHT)                  // check the bottom pixel
 		b_pix = SCREEN_HEIGHT;
 	if (t_pix < 0) // check the top pixel
 		t_pix = 0;
-	draw_floor_ceiling(game, ray_counter, t_pix, b_pix); // draw the floor and the ceiling
-	draw_wall(game, ray_counter, t_pix, b_pix, hit); // draw the wall
+	draw_floor_ceiling(game, ray_counter, t_pix, b_pix);
+		// draw the floor and the ceiling
+	draw_wall(game, ray_counter, t_pix, b_pix, hit);     // draw the wall
 }
